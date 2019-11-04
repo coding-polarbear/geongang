ca602d8cea357af7fe2aa04dc2dc84da7c1ce782
baehynebin <baehynebin@hotmail.co.kr>
2019-11-04 20:37:45 +0900

    분 초 float->int

--- app/src/main/java/com/example/geongang/Converter.kt
+++ app/src/main/java/com/example/geongang/Converter.kt
@@ -1,13 +1,15 @@
 package com.example.geongang
 
+import kotlin.math.roundToInt
+
 object Converter {
     fun convertTime(time: Float): String {
         if(time > 60) {
             var minute = time / 60
             var second = time % 60
-            return "${minute}분 #{second}초"
+            return "${minute.toInt()}분 #{second}초"
         } else {
-            return "${time}초"
+            return "${time.toInt()}초"
         }
     }
 }
